ceb0434e2aabda3b0fcb5aa143af583bc0605b03
Matthew Jordan <mbj755@gmail.com>
2012-02-15 10:23:07 -0500

    Development of 1.1

---
+++ dev/render.js
@@ -0,0 +1,27 @@
+/**
+ New render function to reduce filesize.
+	280 bytes minified (201 bytes min & gzipped)
+*/
+
+render: function (map, type) { // Internal Function
+    var r = [map[0], map[1], map[2]], rtn = [];
+    if (typeof map != 'object') {
+        return;
+    }
+    if (type == 'rgb') {
+        var k = ['R', 'G', 'B', 'RGB'];
+    }
+    if (type == 'hsv') {
+        var k = ['H', 'S', 'V', 'HSV'];
+    }
+    if (type == 'hsl') {
+        var k = ['H', 'S', 'L', 'HSL'];
+    }
+    rtn[k[0]] = r[0];
+    rtn[k[1]] = r[1];
+    rtn[k[2]] = r[2];
+    rtn[k[3]] = r[0] + ' ' + r[1] + ' ' + r[2];
+    rtn['a'] = [r[0], r[1], r[1]];
+    return rtn;
+}
+
