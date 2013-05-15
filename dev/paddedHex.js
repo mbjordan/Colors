11cb9e6e8a2e61bd07d1c7f64b4dab6e36adac40
Matt Jordan <mbj755@gmail.com>
2013-05-15 13:58:58 -0400

    Colors.js v1.2 RC1
    The first release candidate for v1.2

--- dev/paddedHex.js
+++
@@ -1,9 +0,0 @@
-/*
-  Added paddedHex function (internal)
-  The previous method checked for the length of the number before conversion.
-  This will allow checking both before and after conversion, making sure the returned hex has 2 chars.
-*/
-function paddedHex(n) {
-    var hex = ((n < 10) ? '0' : '') + n.toString(16);
-    return (hex.length === 1) ? '0' + hex : hex;
-}
