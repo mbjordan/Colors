2de5f8fdc3a320b39fced40c2e8baa8cecfdf11e
Matt Jordan <majordan@yodle.com>
2015-10-01 11:01:32 -0400

    Renamed colors.js to index in proj. root

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,11 +7,12 @@ var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 
 gulp.task('build', function() {
-    // set up the browserify instance on a task basis
     var b = browserify({
-            entries: './index.js',
-            debug: true
-        }).bundle()
+        entries: './index.js',
+        debug: true
+    });
+
+    return b.bundle()
         .pipe(source('colors.min.js'))
         .pipe(buffer())
         .pipe(sourcemaps.init({
@@ -19,8 +20,6 @@ gulp.task('build', function() {
         }))
         .pipe(uglify())
         .on('error', gutil.log)
-        .pipe(sourcemaps.write('./'))
+        // .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dist/'));
-
-    return b;
 });
